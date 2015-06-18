974609c2c8a190050fac09b6adf880a1aac78eff
Tyler Pearson <ty.pearson@gmail.com>
2015-06-17 23:33:25 -0700

    Add initial TOCs endpoint

--- server.go
+++ server.go
@@ -2,10 +2,10 @@ package main
 
 import (
     "database/sql"
+    "github.com/coopernurse/gorp"
     "github.com/go-martini/martini"
     _ "github.com/go-sql-driver/mysql"
     "github.com/martini-contrib/render"
-    "github.com/coopernurse/gorp"
     "log"
 )
 
@@ -16,8 +16,30 @@ func checkErr(err error, msg string) {
 }
 
 type Code struct {
-    Code        string `db:"code" json:"code"`
-    Title       string `db:"title" json:"title"`
+    Code  string `db:"code" json:"code"`
+    Title string `db:"title" json:"title"`
+}
+
+type Toc struct {
+    LawCode             string  `db:"law_code" json:"law_code"`
+    Division            *string `db:"division" json:"division"`
+    Title               *string `db:"title" json:"title"`
+    Part                *string `db:"part" json:"part"`
+    Chapter             *string `db:"chapter" json:"chapter"`
+    Article             *string `db:"article" json:"article"`
+    Heading             *string `db:"heading" json:"heading"`
+    Active              *string `db:"active_flg" json:"active"`
+    TransUID            *string `db:"trans_uid" json:"trans_uid"`
+    TransUpdate         *string `db:"trans_update" json:"trans_update"`
+    NodeSequence        *string `db:"node_sequence" json:"node_sequence"`
+    NodeLevel           *string `db:"node_level" json:"node_level"`
+    NodePosition        *string `db:"node_position" json:"node_position"`
+    NodeTreePath        *string `db:"node_treepath" json:"node_treepath"`
+    ContainsLawSections *string `db:"contains_law_sections" json:"contains_law_sections"`
+    HistoryNote         *string `db:"history_note" json:"history_note"`
+    OpStatues           *string `db:"op_statues" json:"op_statues"`
+    OpChapter           *string `db:"op_chapter" json:"op_chapter"`
+    OpSection           *string `db:"op_section" json:"op_section"`
 }
 
 var dbmap *gorp.DbMap
@@ -31,7 +53,6 @@ func initDb() *gorp.DbMap {
     return dbmap
 }
 
-
 func main() {
     m := martini.Classic()
 
@@ -42,7 +63,7 @@ func main() {
     defer dbmap.Db.Close()
 
     m.Get("/", func() string {
-        return "hello world"
+        return "Welcome to the California Laws API!"
     })
 
     m.Group("/api", func(r martini.Router) {
@@ -58,8 +79,20 @@ func main() {
             }
         })
 
+        m.Get("/tocs", func(r render.Render) {
+            var tocs []Toc
+
+            _, err := dbmap.Select(&tocs, "SELECT law_code, division, title, part, chapter, article, heading, active_flg, trans_uid, trans_update, node_sequence, node_level, node_position, node_treepath, contains_law_sections, history_note, op_chapter, op_section FROM law_toc_tbl LIMIT 100")
+
+            if err == nil {
+                r.JSON(200, tocs)
+            } else {
+                log.Fatal(err)
+                r.JSON(404, map[string]interface{}{"error": true})
+            }
+        })
+
     })
 
     m.Run()
 }
-
